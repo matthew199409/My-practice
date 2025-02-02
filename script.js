2aefdd10c04ad03c9fb9c5812f3f40907ff95c8d
matthew199409 <heraldmatthew298@gmail.com>
2025-02-02 16:39:40 -0500

    comments added

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
-let helloButton = document.getElementsByClassName("button")[1];
+// Select the second button with class "button"
+const helloButton = document.getElementsByClassName("button")[1];
 
 console.log(helloButton);
 
+// Add click event listener to the helloButton
 helloButton.addEventListener("click", () => {
+  // Toggle background color between green and red
   if (helloButton.style.backgroundColor === "green") {
     helloButton.style.backgroundColor = "red";
   } else {
     helloButton.style.backgroundColor = "green";
   }
-  document.getElementsByClassName("button")[0].innerHTML = "I'm Gay";
+
+  // Change the text of the first button with class "button"
+  const firstButton = document.getElementsByClassName("button")[0];
+  firstButton.innerHTML = "I'm Gay";
+
+  // Change the text content if the condition is met
   if (helloButton.innerHTML === " I'm Gay") {
-    helloButton.textContent[0] = "Nuh uh";
+    helloButton.textContent = "Nuh uh";
   }
 });
 
+// Function to change the text of the element with id "demo"
 function myFunction() {
-  document.getElementById("demo").innerHTML = "It changed!!!";
-  if (demo.innerHTML === "It changed!!!") {
-    demo.textContent = "You're Correct!";
-  } else {
-    demo.innerHTML = "ugh";
+  const demoElement = document.getElementById("demo");
+  if (demoElement) {
+    demoElement.innerHTML = "It changed!!!";
+    if (demoElement.innerHTML === "It changed!!!") {
+      demoElement.textContent = "You're Correct!";
+    } else {
+      demoElement.innerHTML = "ugh";
+    }
   }
 }
 
+// Function to change the text and background color of the element with id "demo2"
 function myFunction2() {
-  document.getElementById("demo2").innerHTML = "There it goes";
-  if (demo2.style.backgroundColor === "red") {
-    demo2.style.backgroundColor = "yellow";
-  } else {
-    demo2.style.backgroundColor = "red";
-    demo2.innerHTML = "You're Queer!!!";
+  const demo2Element = document.getElementById("demo2");
+  if (demo2Element) {
+    demo2Element.textContent = "There it goes";
+    if (demo2Element.style.backgroundColor === "red") {
+      demo2Element.style.backgroundColor = "yellow";
+    } else {
+      demo2Element.style.backgroundColor = "red";
+    }
   }
-
-  document.getElementsByClassName("button")[0].innerHTML = "your gay";
 }
 
-function btnFunction() {
-  document.getElementsByClassName("heading3")[0].innerHTML = "Duhhh...";
-}
-
+// Function to get a response based on the user's input
 function getResponse() {
   const questionInput = document.getElementById("questionInput").value;
   const responseElement = document.getElementById("response");
+
   // Simple response logic
   let response;
   if (questionInput.toLowerCase().includes("hello")) {
